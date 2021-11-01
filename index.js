6649aedc55a51b89e5d1c7610237aff526fae003
Sagnik Ghosh <77972501+SagnikGhosh18@users.noreply.github.com>
2021-11-02 00:07:50 +0530

    Add files via upload
    Version 1

---
+++ index.js
@@ -0,0 +1,20 @@
+var arr1 = ["dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png"];
+
+var random1 = Math.ceil(Math.random()*6);
+document.querySelector(".img1").setAttribute("src",arr1[random1 -1]);
+
+var random2 = Math.ceil(Math.random()*6);
+document.querySelector(".img2").setAttribute("src",arr1[random2 -1]);
+
+if(random1===random2)
+{
+  document.querySelector("h1").innerHTML="It's a draw!🙌";
+}
+else if (random1<random2)
+{
+  document.querySelector("h1").innerHTML="Player 2 wins!🏁";
+}
+else
+{
+  document.querySelector("h1").innerHTML="🏁 Player 1 wins!";
+}
