3a4a480ef62c19ce51f9ab5848039504696f601f
Sagnik Ghosh <77972501+SagnikGhosh18@users.noreply.github.com>
2021-11-03 15:10:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var arr1 = ["dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png"];
+var str = prompt("Start simulation?- (Y/N)");
 
-var random1 = Math.ceil(Math.random()*6);
-document.querySelector(".img1").setAttribute("src",arr1[random1 -1]);
+if(str=="Y")
+{
+  var arr1 = ["dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png"];
 
-var random2 = Math.ceil(Math.random()*6);
-document.querySelector(".img2").setAttribute("src",arr1[random2 -1]);
+  var random1 = Math.ceil(Math.random()*6);
+  document.querySelector(".img1").setAttribute("src",arr1[random1 -1]);
 
-if(random1===random2)
-{
-  document.querySelector("h1").innerHTML="It's a draw!🙌";
-}
-else if (random1<random2)
-{
-  document.querySelector("h1").innerHTML="Player 2 wins!🏁";
-}
-else
-{
-  document.querySelector("h1").innerHTML="🏁 Player 1 wins!";
+  var random2 = Math.ceil(Math.random()*6);
+  document.querySelector(".img2").setAttribute("src",arr1[random2 -1]);
+
+  if(random1===random2)
+  {
+    document.querySelector("h1").innerHTML="It's a draw!🙌";
+  }
+  else if (random1<random2)
+  {
+    document.querySelector("h1").innerHTML="Player 2 wins!🏁";
+  }
+  else
+  {
+    document.querySelector("h1").innerHTML="🏁 Player 1 wins!";
+  }
 }
